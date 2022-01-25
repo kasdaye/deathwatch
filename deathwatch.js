4677c00bbfd26c4f8d33d5284d125dbfcd99dc56
sam-bandyopadhyay <rwijub@gmail.com>
2022-01-25 16:53:58 -0700

    Show shoulder pads of chapters with special characters correctly

--- deathwatch.js
+++ deathwatch.js
@@ -72,7 +72,7 @@ async function preloadHandlebarsTemplates() {
 }
 
 function camelize(str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+    return str.replace(/\W/g, '').replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
         return index === 0 ? word.toLowerCase() : word.toUpperCase();
     }).replace(/\s+/g, '');
 }
